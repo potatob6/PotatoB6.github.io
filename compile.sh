0f7c9e0a0de33a30cf45a85a8d8e54b05e8e9a9b
PotatoB <1363964141@qq.com>
2021-10-21 19:25:37 +0800

    添加wget

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
+if [ ! -f "mybochsrc" ]; then
+    wget https://potatob666.github.io/mybochsrc
+fi
 dd if=/dev/zero of=myhd.img bs=512 count=10240
 nasm -f bin a.asm -o a.bin
 dd if=a.bin of=myhd.img bs=512 count=1 conv=notrunc
