5718bbd5fede8a03149621524602ba681cca53b0
PotatoB666 <1363964141@qq.com>
2021-11-24 21:42:31 +0800

    增加可以修改文件名的compile.sh

--- compile.sh
+++ compile.sh
@@ -18,14 +18,38 @@ if [[ ! (-e "readfile-armv7l" || -e "readfile-x86_64" ) ]]; then
 	chmod +x readfile-$arch
 fi
 chmod +x $0
+filename="a"
+paramCounts=$#
+fullFileName="a.asm"
+onlyCompile=0
+for i in $(seq $paramCounts):
+do
+	if [ $1 == "-c" ]; then
+		onlyCompile=1
+	elif [ $1 == "-h" ]; then
+		echo "Usage: $0 [fileName] [option]"
+		echo "        options:"
+		echo "              -c: only compile"
+		echo "              -h: show this hints"
+		echo ""
+		echo "        Using a.asm as file name If there is no file indicated."
+		exit 0
+	else
+		fullFileName=$1
+		filename=$(echo $fullFileName|cut -f 1 -d '.')
+	fi
+	shift
+done
+echo "Full name is $fullFileName"
+echo "File name is $filename"
 dd if=/dev/zero of=myhd.img bs=512 count=10240
-nasm -f bin a.asm -o a.bin
-if [ ! $? ]; then
-    echo "nasm出现错误"
+nasm -f bin $fullFileName -o $filename.bin
+if [ $? != "0" ]; then
+    echo "nasm encounter a error"
     exit 1
 fi
 dd if=a.bin of=myhd.img bs=512 count=1 conv=notrunc
-if [ $# -lt 1 ]; then
+if [ $onlyCompile -eq 0 ]; then
 	bochs -f mybochsrc
 else
 	echo "compile and no run"
